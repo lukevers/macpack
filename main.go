c49bb50e33d6e08f17b607e9f805bdc228cabd7e
Maxence Charriere <charriere@outlook.com>
2016-12-05 02:25:43 -0800

    flags

--- main.go
+++ main.go
@@ -1,4 +1,60 @@
 package main
 
+import (
+	"os"
+
+	"flag"
+
+	"github.com/murlokswarm/log"
+)
+
 func main() {
+	var force bool
+	var verb bool
+
+	flag.BoolVar(&force, "f", false, "Forces the copy of the whole resources directory rather than sync it.")
+	flag.BoolVar(&verb, "v", false, "Verbose mode.")
+	flag.Parse()
+
+	if err := build(false); err != nil {
+		log.Error(err)
+		return
+	}
+
+	conf, err := readConfig(confName)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			log.Error(err)
+			return
+		}
+
+		conf = defaultConfig()
+		err = saveConfig(conf, confName)
+	}
+
+	if err = createPackage(conf); err != nil {
+		log.Error(err)
+		return
+	}
+
+	if err = createExec(conf); err != nil {
+		log.Error(err)
+		return
+	}
+
+	if err = createPlist(conf); err != nil {
+		log.Error(err)
+		return
+	}
+
+	if err = syncResources(conf, force, verb); err != nil {
+		log.Error(err)
+		return
+	}
+
+	if len(conf.Icon) != 0 {
+		if err = generateIcon(conf); err != nil {
+			log.Error(err)
+		}
+	}
 }
